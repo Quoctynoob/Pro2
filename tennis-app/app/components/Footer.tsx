7ff65952938192e91e20a87cb2da9c2218f7203e
unknown <tilburgquoc34@gmail.com>
2024-07-16 20:49:02 +0300

    Change

--- tennis-app/app/components/Footer.tsx
+++ tennis-app/app/components/Footer.tsx
@@ -5,7 +5,6 @@ const Footer = () => {
     <section className="bg-green-100 p-4 text-black w-full snap-start">
       <div className="container mx-auto">
         <p className='text-left'>Quoc Le™. All Right Reserved.</p>
-        
       </div>
     </section>
   );
