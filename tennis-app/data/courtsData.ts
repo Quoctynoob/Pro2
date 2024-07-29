222457643f467789cb518c1fbbfc606a52cb6942
unknown <tilburgquoc34@gmail.com>
2024-07-29 19:19:34 +0300

    Finish data structure courts

--- tennis-app/data/courtsData.ts
+++ tennis-app/data/courtsData.ts
@@ -48,4 +48,130 @@ export const courts = [
       lat: 43.533452721108056, 
       lng: -80.27750057846698,
     },
+
+    {
+      id: '7',
+      name: 'Parkway Valley TC',
+      image: 'https://lh3.googleusercontent.com/p/AF1QipO6fx9gu6jjWAIpXUXZqKdyue2cmDAZBrfBTv0q=s680-w680-h510',
+      tags: ['Toronto','Hard', 'Outdoor'],
+      lat: 43.76174942207438,
+      lng: -79.31846318182431,
+    },
+
+    {
+      id: '8',
+      name: 'Ramsden Park Tennis Courts',
+      image: 'https://www.toronto.ca/data/parks/img/167/3.jpg',
+      tags: ['Toronto','Asphalt', 'Outdoor'],
+      lat: 43.67977641428939, 
+      lng: -79.39501463193149,
+    },
+
+    {
+      id: '9',
+      name: 'Margaret Greene Park',
+      image: 'https://lh3.googleusercontent.com/p/AF1QipMTWKmVWbHNLKawMlu_8eg1jxPsF8uZkD-Qb6Ov=s680-w680-h510',
+      tags: ['Guelph','Hard', 'Outdoor'],
+      lat: 43.533452721108056, 
+      lng: -80.27750057846698,
+    },
+
+    {
+      id: '10',
+      name: 'Dentonia Park Tennis Club',
+      image: 'https://lh3.googleusercontent.com/p/AF1QipNrg4X9MEW1mOFcPEbZHbqttNmbvbWfTr4lodhk=s680-w680-h510',
+      tags: ['Toronto','Hard', 'Outdoor'],
+      lat: 43.69742984355095, 
+      lng: -79.29309288464965,
+    },
+
+    {
+      id: '11',
+      name: 'Davisville Tennis Club',
+      image: 'https://lh3.googleusercontent.com/p/AF1QipM-w7ucBuPrvk2QOpoedk2IvVHw1rpmAig83qH5=s680-w680-h510',
+      tags: ['Toronto','Hard', 'Outdoor'],
+      lat: 43.70687508414337, 
+      lng: -79.38863083393431,
+    },
+
+    {
+      id: '12',
+      name: 'Toronto Lawn Tennis Club',
+      image: 'https://lh3.googleusercontent.com/p/AF1QipPzsTp6r_BdUbDA6GEDUwpO0YNcEToe3F91hmpP=s680-w680-h510',
+      tags: ['Toronto','Hard', 'Indoor'],
+      lat: 43.68827834769968, 
+      lng: -79.38658942476157,
+    },
+
+    {
+      id: '13',
+      name: 'Riverdale Park Tennis Courts',
+      image: 'https://www.ehcanadatravel.com/images/sobipro/entries/8263/img_riverdaleparkeastsports.jpg',
+      tags: ['Toronto','Asphalt', 'Outdoor'],
+      lat: 43.676351422847524, 
+      lng: -79.35661663311289,
+    },
+
+    {
+      id: '14',
+      name: 'KEW Garden Tennis Club',
+      image: 'https://lh3.googleusercontent.com/p/AF1QipNcthBIcIv6cOPRAzTtUMinVgGyYS7HMhXPCwMA=s680-w680-h510',
+      tags: ['Toronto','Hard', 'Outdoor'],
+      lat: 43.67125935954562, 
+      lng: -79.30150878941085,
+    },
+
+    {
+      id: '15',
+      name: 'North Toronto Tennis Club',
+      image: 'https://lh3.googleusercontent.com/p/AF1QipML299J_zCKvR6nhsNknFAvCZZVoHNV5Gur0iBy=s680-w680-h510',
+      tags: ['Toronto','Hard', 'Outdoor'],
+      lat: 43.72009258885859, 
+      lng: -79.40944401386062,
+    },
+
+    {
+      id: '16',
+      name: 'Cedarvale Park Tennis Courts',
+      image: 'https://lh3.googleusercontent.com/p/AF1QipMzKphqSKrkcAMevgUX2F3_Bup3Ngz9OZO3o9yS=s680-w680-h510',
+      tags: ['Toronto','Hard', 'Outdoor'],
+      lat: 43.69850157519951, 
+      lng: -79.43278995957442,
+    },
+
+    {
+      id: '17',
+      name: 'The Supreme Court Tennis Club',
+      image: 'https://lh3.googleusercontent.com/p/AF1QipP0M0PXLtb3hVZOGZNm8ftTVnbzAytzbaCVoC0d=s680-w680-h510',
+      tags: ['Toronto','Hard', 'Indoor'],
+      lat: 43.77936580076538, 
+      lng: -79.32704655839987,
+    },
+
+    {
+      id: '18',
+      name: 'Seven Oaks Tennis Club',
+      image: 'https://lh3.googleusercontent.com/p/AF1QipNSnDCuOyPAurispZNnTsEG1WNhMATsNb4KKEZO=s680-w680-h510',
+      tags: ['Toronto','Hard', 'Outdoor'],
+      lat: 43.79350799176, 
+      lng: -79.21109047777769,
+    },
+
+    {
+      id: '19',
+      name: 'Gwendolen Park Tennis Club',
+      image: 'https://lh3.googleusercontent.com/p/AF1QipMoKi2ok_s6Bq8xMuATIAg8wLf2JsjACxRg-zYW=s680-w680-h510',
+      tags: ['Toronto','Hard', 'Outdoor'],
+      lat: 43.75742315229006, 
+      lng: -79.4208490323155,
+    },
+
+    {
+      id: '20',
+      name: 'Sir Winston Churchill Park Tennis Club',
+      image: 'https://lh3.googleusercontent.com/p/AF1QipN0XKXvXhfyWhcQZsrUMGCg38WqHU1DTD-1Zzcq=s680-w680-h510',
+      tags: ['Toronto','Hard', 'Outdoor'],
+      lat: 43.69043292615358, 
+      lng: -79.40883273528982,
+    },
   ];
